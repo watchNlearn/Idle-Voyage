513f3ba0d5ddfb3cac54092c769e9992942d8be5
Caleb Lee <28caleblee@gmail.com>
2022-02-02 19:36:58 -0800

    progress detail

--- Idle Voyage/HomeDetailViews/ProgressDetail.swift
+++ Idle Voyage/HomeDetailViews/ProgressDetail.swift
@@ -8,20 +8,50 @@
 import SwiftUI
 
 struct ProgressDetail: View {
+    
+    let timer = Timer.publish(every: 1, tolerance: 0.1, on: .main, in: .common).autoconnect()
+    
     @FetchRequest(sortDescriptors: []) var user: FetchedResults<User>
 
-    
+    @State var progressValue: Float = 0.0
+
     var body: some View {
         VStack {
             Text("Voyage Progress  " + user.first!.shipPref!)
                 .font(.title)
                 .fontWeight(.bold)
-            ScrollView(.horizontal, showsIndicators: false) {
-                HStack(spacing: 16) {
-                    ForEach(0...spaceObjectsSorted.count-1, id: \.self) { index in
-                        PlanetDetailCard(spaceObject: spaceObjectsSorted[index])
+            ScrollViewReader { proxy in
+                ScrollView(.horizontal, showsIndicators: false) {
+                    HStack(spacing: 16) {
+                        ForEach(0...spaceObjectsSorted.count-1, id: \.self) { index in
+                            PlanetDetailCard(spaceObject: spaceObjectsSorted[index])
+                                .id(spaceObjectsSorted[index].name)
+                        }
                     }
                 }
+                .onAppear {
+                    let lastSpaceObj = getLastSpaceObject(spaceObjects: spaceObjectsSorted, distance: user.first!.distanceInKm)
+                    proxy.scrollTo(lastSpaceObj.name)
+                }
+            }
+            
+            HStack {
+                Image(uiImage: getLastSpaceObject(spaceObjects: spaceObjectsSorted, distance: user.first!.distanceInKm).image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                    .frame(width: 50)
+                
+                OnboardingWidgetPbar(value: $progressValue).frame(maxWidth: .infinity)
+                    .frame(height: 30)
+                    .onReceive(timer) { _ in
+                        progressValue = user.first?.progress ?? 0
+                    }
+                Image(uiImage: getNextSpaceObject(spaceObjects: spaceObjectsSorted, distance: user.first!.distanceInKm).image)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(height: 50)
+                    .frame(width: 50)
             }
             
         }
